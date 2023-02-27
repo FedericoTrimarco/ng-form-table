2038de2f9e57b6d3cd32c3fc444ffca9254692ea
FedericoTrimarco <trimarcofederico98@gmail.com>
2023-02-28 00:47:09 +0100

    end setup

--- src/app/smart-table/smart-table.component.ts
+++ src/app/smart-table/smart-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'app-smart-table',
@@ -6,43 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./smart-table.component.scss']
 })
 export class SmartTableComponent implements OnInit{
-  settings = {
-    columns: {
-      id: {
-        title: 'ID'
-      },
-      name: {
-        title: 'Full Name'
-      },
-      username: {
-        title: 'User Name'
-      },
-      email: {
-        title: 'Email'
-      }
-    }
-  };
+  settings: any;
+  sourceValori: LocalDataSource = new LocalDataSource;
   
   data = [
     {
       id: 1,
       name: "Leanne Graham",
       username: "Bret",
+      address: 'via del test 34',
+      job: 'web developer',
       email: "Sincere@april.biz"
     },
-    // ... other rows here
     {
       id: 11,
       name: "Nicholas DuBuque",
       username: "Nicholas.Stanton",
+      address: 'via del test 34',
+      job: 'web developer',
       email: "Rey.Padberg@rosamond.biz"
-    }
+    },
+    {
+      id: 13,
+      name: "Diego Salvati",
+      username: "Diego.Salvati",
+      address: 'via del test 34',
+      job: 'web developer',
+      email: "Rey.Padberg@rosamond.biz"
+    },
+    {
+      id: 15,
+      name: "Luca Laurenti",
+      username: "Luca.Laurenti",
+      address: 'via del test 34',
+      job: 'web developer',
+      email: "Rey.Padberg@rosamond.biz"
+    },
   ];
-  constructor(){
+
+  constructor(
+    private router: Router,
+  ){
 
   }
 
   ngOnInit(): void {
-    
+    // 3) inserire una funzione che richiami sia il setup dei settings che il caricamento dei dati all'interno di source valori
   }
+
+  routeTo(path: any) {
+    this.router.navigate([path]);
+  }
+
+  // 1) creare una funzione per i settings
+
+
+  // 2) creare una funziona che carichi i dati all'interno della variabile sourceValori
 }
